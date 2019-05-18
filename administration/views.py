abc3d1eaa064962aca0044fc4e4b94441b9f8c96
sajadrezvani <Rezvani@ce.sharif.edu>
2019-05-18 14:24:28 +0430

    fix bug

--- administration/views.py
+++ administration/views.py
@@ -177,45 +177,73 @@ def get_orders(request):
 @api_view(['GET'])
 @login_required(login_url='/admin/')
 def get_order_excel(request):
-    try:
-        response = HttpResponse(content_type='application/ms-excel')
-        response['Content-Disposition'] = 'attachment; filename="orders.xls"'
+    # try:
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="orders.xls"'
 
-        wb = xlwt.Workbook(encoding='utf-8')
-        ws = wb.add_sheet('سفارشات')
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('گزارشات')
 
-        font_style = xlwt.XFStyle()
-        font_style.font.bold = True
+    ws.cols_right_to_left = 1
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
 
-        columns = [
-            'نام خریدار',
-            'کد سفارش',
-            'استان',
-            'شهر',
-            'آدرس خریدار',
-            'قیمت',
-            'تاریخ سفارش',
-            'تاریخ تغییر',
-            'تاریخ تایید',
-            'تاریخ ارسال',
-            'تاریخ تحویل',
-            'وضعیت',
-        ]
+    columns = [
+        'نام خریدار',
+        'کد سفارش',
+        'استان',
+        'شهر',
+        'آدرس خریدار',
+        'قیمت',
+        'تاریخ سفارش',
+        'تاریخ تغییر',
+        'تاریخ تایید',
+        'تاریخ ارسال',
+        'تاریخ تحویل',
+        'وضعیت',
+    ]
 
-        row_name = 0
+    row_name = 0
 
-        for col in range(len(columns)):
-            ws.write(row_name, col, columns[col], font_style)
+    for col in range(len(columns)):
+        ws.write(row_name, col, columns[col], font_style)
 
-        font_style = xlwt.XFStyle()
+    font_style = xlwt.XFStyle()
 
-        orders = Order.objects.all()
-        for order in orders:
-            row_name += 1
-            ws.write(row_name, columns[0], order.customer.company_name, font_style)
-            ws.write(row_name, columns[0], order.customer.company_name, font_style)
+    orders = Order.objects.filter(is_checked_out=True).all().order_by('-created_date')
+    for order in orders:
+        row_name += 1
+        ws.write(row_name, 0, order.customer.company_name, font_style)
+        ws.write(row_name, 1, order.order_id, font_style)
+        ws.write(row_name, 2, '', font_style)  # TODO add state
+        ws.write(row_name, 3, order.customer.city, font_style)
+        ws.write(row_name, 4, order.customer.address, font_style)
+        ws.write(row_name, 5, order.cost, font_style)
+        ws.write(row_name, 6, jalali.Gregorian(order.created_date.strftime('%Y-%m-%d')).persian_string(),
+                 font_style)
+        if order.last_change_date:
+            ws.write(row_name, 7, jalali.Gregorian(order.last_change_date.strftime('%Y-%m-%d')).persian_string(),
+                     font_style)
+        if order.confirmed_date:
+            ws.write(row_name,8, jalali.Gregorian(order.confirmed_date.strftime('%Y-%m-%d')).persian_string(),
+                     font_style)
+        if order.sent_date:
+            ws.write(row_name, 9, jalali.Gregorian(order.sent_date.strftime('%Y-%m-%d')).persian_string(),
+                     font_style)
+        if order.received_date:
+            ws.write(row_name, 10, jalali.Gregorian(order.received_date.strftime('%Y-%m-%d')).persian_string(),
+                     font_style)
+        if order.is_received:
+            ws.write(row_name, 11, 'تحویل شده',
+                     font_style)
+        elif order.is_confirmed:
+            ws.write(row_name, 11, 'تایید شده',
+                     font_style)
+        else:
+            ws.write(row_name,11, 'تایید نشده',
+                     font_style)
 
-        wb.save(response)
-        return response
-    except:
-        return Response({'msg': 'مشکلی در سرور به وجود آمده'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    wb.save(response)
+    return response
+    # except:
+    #     return Response({'msg': 'مشکلی در سرور به وجود آمده'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
